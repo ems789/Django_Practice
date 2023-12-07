c3f5daad10abb0aae5474dfd34b4161166a95820
ems789 <ems789@naver.com>
2023-12-07 17:55:30 +0900

    로그아웃 상태에서는 답변을 달 수 없게 수정

--- pybo/views.py
+++ pybo/views.py
@@ -34,7 +34,7 @@ def answer_create(request, question_id):
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
