ec843baac2b68ff8581d273128eee5dc7575df3f
ems789 <ems789@naver.com>
2023-12-07 12:19:10 +0900

    답변등록 폼 수정

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 class QuestionForm(forms.ModelForm): # 모델 폼을 상속
     class Meta: # 모델 폼은 이너 클래스인 Meta 클래스가 반드시 필요하다
@@ -9,4 +9,12 @@ class QuestionForm(forms.ModelForm): # 모델 폼을 상속
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
         }
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.http import HttpResponseNotAllowed
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by("-create_date") # 조회 결과를 정렬하는 함수(-create_date)는 작성일시 역순 정렬을 의미
@@ -16,15 +17,18 @@ def detail(request, question_id):
 
 def answer_create(request, question_id): 
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-create_date=timezone.now())
-    # 답변 등록시 텍스트창에 입력한 내용은 request 객체를 통해 읽을 수 있다 (request.POST.get('content'))
-
-    # 답변을 생성하기 위해 question.answer_set.create를 사용
-    # Question과 Answer 모델은 서로 ForeignKey로 연결되어 있기 때문에 이처럼 사용할 수 있다.
-
-    return redirect('pybo:detail', question_id=question.id)
-    # 답변 생성 후 질문 페이지를 다시 보여주기 위해 redirect 함수를 사용.
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 def question_create(request):
     # URL 요청을 POST, GET 요청 방식에 따라 다르게 처리
