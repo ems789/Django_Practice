45406c1a06c51e1e453cbee7836b56a1f5b2af7d
ems789 <ems789@naver.com>
2023-12-07 15:36:57 +0900

    로그인 기능 추가

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
 app_name = 'common'
 
 urlpatterns = [
-    
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'), 
+    # loginView는 template_name을 지정해주지 않으면 
+    # registration이라는 템플릿 디렉터리에서 login.html 파일을 찾는다.
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
