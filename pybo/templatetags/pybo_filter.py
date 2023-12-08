1ac5111ee3c5cba9e408a209a6c7bbaff5dec8f1
ems789 <ems789@naver.com>
2023-12-08 10:50:41 +0900

    마크다운 필터 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,7 +1,15 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter # 템플릿에서 해당 함수를 필터로 사용할 수 있게 해줌
 def sub(value, arg):
     return value - arg
+
+# 입력 문자열을 HTML로 변환하는 필터 함수
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
